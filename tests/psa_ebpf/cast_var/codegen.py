138d871da9c8df08ac8d2c48f540a323dcd1b8c2
Alexander Weingart <a.weingart@campus.tu-berlin.de>
2023-06-25 16:19:37 +0200

    CastVar Test
    adds implementation of a simple codegen testing the CastVar command
    (
    casts uint8 to uint64 and then shifts and adds the original
    example i/o:
    in: A
    out: AAAAA
    )

---
+++ tests/psa_ebpf/cast_var/codegen.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('../../../src')
+
+from p4rrot.generator_tools import *
+from p4rrot.known_types import *  
+from p4rrot.standard_fields import *
+from p4rrot.core.commands import *  
+
+UID.reset()
+fp = FlowProcessor(
+        istruct = [('i_uint8', uint8_t)],
+        mstruct = [('temp_uint64', uint64_t)],
+        ostruct = [('o_uint64', uint64_t)]
+)
+
+
+
+(
+fp
+.add(CastVar('o_uint64', 'i_uint8'))
+.add(CastVar('temp_uint64', 'i_uint8'))
+.add(LeftShift('o_uint64', 8))
+.add(StrictAddition('o_uint64', 'o_uint64', 'temp_uint64'))
+.add(LeftShift('o_uint64', 8))
+.add(StrictAddition('o_uint64', 'o_uint64', 'temp_uint64'))
+.add(LeftShift('o_uint64', 8))
+.add(StrictAddition('o_uint64', 'o_uint64', 'temp_uint64'))
+.add(SendBack())
+)
+
+
+fs = FlowSelector(
+        'IPV4_UDP',
+        [(UdpDstPort,5555)],
+        fp
+    )
+
+
+solution = Solution()
+solution.add_flow_processor(fp)
+solution.add_flow_selector(fs)
+solution.get_generated_code().dump('template')
